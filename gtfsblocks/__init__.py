42a8e32612222c5caf5deae55a87f41789b86425
Dan McCabe <danmccabe17@gmail.com>
2025-04-07 15:00:18 -0700

    define __all__ in __init__.py

--- gtfsblocks/__init__.py
+++ gtfsblocks/__init__.py
@@ -4,5 +4,14 @@ from .gtfs import (
     get_shape,
     plot_trips_and_terminals,
     filter_blocks_by_route,
-    get_all_trip_data
+    get_all_trip_data,
 )
+
+__all__ = [
+    "Feed",
+    "add_deadhead",
+    "get_shape",
+    "plot_trips_and_terminals",
+    "filter_blocks_by_route",
+    "get_all_trip_data",
+]
